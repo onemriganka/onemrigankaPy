69ef86dba23ed85740ab34efb4e33c55f10245d5
Mriganka Barman <mribarman215@gmail.com>
2024-08-01 22:38:27 +0500

    added xtimes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['PyAutoGUI',],
     keywords=['onemriganka'],
     classifiers=[
         "Development Status :: 1 - Planning",
